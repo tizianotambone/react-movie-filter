aa0e880785d300b40c80ecd6e71f40d749f72e71
tizianotambone <tambonetiziano@gmail.com>
2025-05-12 19:18:10 +0200

    [UPD]ex done

--- react-movie-filter/src/components/ListFilm.jsx
+++ react-movie-filter/src/components/ListFilm.jsx
@@ -11,39 +11,36 @@ const FilmList = [
     { title: 'Interstellar', genre: 'Fantascienza' },
     { title: 'Pulp Fiction', genre: 'Thriller' },
 ]
-
-
-
 const ListFilm = () => {
-    let [film, getFilm] = useState(FilmList);
+    let [film, setFilm] = useState(FilmList);
 
     const [value, setValue] = useState("");
     const cambioValore = (e) => {
         setValue(e.target.value)
     }
 
-    const setSearch = (value) => {
-        const filteredFilm = film.filter((film) => {
-            return film.genre.toLowerCase().includes(value.toLowerCase())
-            if (value === ListFilm) {
-                
-            }
-        })   
-        film = filteredFilm;
-        
+   const setSearch = () => {
+    if (value === "") {
+        setFilm(FilmList);
+    } else {
+        const filteredFilm = FilmList.filter((film) =>
+            film.genre.toLowerCase().includes(value.toLowerCase())
+        );
+        setFilm(filteredFilm);
     }
+};
 
     return (
         <>
             <div className="contol">
-                <select class="form-select" aria-label="Default select example" onChange={cambioValore}>
-                    <option selected className='select'>Seleziona il genere</option>
+                <select className="form-select" aria-label="Default select example" onChange={cambioValore}>
+                    <option selected className='select' value="">Seleziona il genere</option>
                     <option value="fantascienza">Fantascienza</option>
                     <option value="thriller">Thriller</option>
                     <option value="romantico">Romantico</option>
                     <option value="azione">Azione</option>
                 </select>
-                <button className='search' onClick={setSearch(value)}> cerca</button>
+                <button className='search' onClick={ setSearch}> cerca</button>
             </div>
 
             {
@@ -57,6 +54,8 @@ const ListFilm = () => {
         </>
     )
 }
+
+
 
 export default ListFilm
 
